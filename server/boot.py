9a0c86ed32dcfd05def5f4dbc6e72d6ba58894fb
Alex McGill <alex.mcgill@outlook.com>
2018-10-02 20:44:43 +0100

    Implemented display route for loading json files

--- server/boot.py
+++ server/boot.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, jsonify
 from flask_bootstrap import Bootstrap
-
+import os
+import json
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -12,7 +13,18 @@ def index():
 def test(input=None):
 	data = {'input': input}
 	return render_template('index.html', data=data)
-	
+
+@app.route('/display/')
+def display():
+	filename = os.path.join(app.static_folder, 'data/ss-top100forever.json')
+	with open(filename) as data:
+		stats = json.load(data)
+		return jsonify(stats)
+	return "Failed to load data"
+
+@app.route('/send/', methods=['GET','POST'])
+def send():
+	return "Send JSON"	
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
