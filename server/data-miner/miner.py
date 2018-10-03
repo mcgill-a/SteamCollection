8c1cb728f09919dba1a51d42202623f9c5525eed
Alex McGill <alex.mcgill@outlook.com>
2018-10-03 22:27:13 +0100

    Made progress on steam web api data miner

--- server/data-miner/miner.py
+++ server/data-miner/miner.py
@@ -39,26 +39,31 @@ def get_game_ids(apps):
 	return ids
 
 def get_steam_api_data(ids):
+	count = 0
 	collection = []
 	for current in ids:
+		count += 1	
 		url ="https://store.steampowered.com/api/appdetails?appids=" + str(current) + "&cc=gb&l=en"
-		print url
+		print count, url
 		# Steam API limits may occur (estimated to be around 100 per 5 minutes)
 		response = urllib.urlopen(url)
 		data = json.loads(response.read())
-		collection.append(data)
-	print "JSON filed retrieved from Steam Web API"
+		#collection.append(data)
+		with open('append.json', 'a') as outfile:
+			json.dump(data, outfile, indent=4, separators=(',', ':'))
+		print "JSON file retrieved from Steam Web API"
+
 	return collection
 
 
 def write_to_file(collection):
 	with open('output.json', 'w') as outfile:
-		json.dump(collection, outfile)
+		json.dumps(collection, outfile, sort_keys=True, indent=4)
 	print "Data has been written to JSON file"
 
 apps = get_game_ids(get_json_file())
 collection = get_steam_api_data(apps)
-write_to_file(collection)
+#write_to_file(collection)
 
 print("Miner Completed")
 print "--------------------"
