2edb522d85483a1877ac419585e5a752d7d6e2d1
Alex McGill <alex.mcgill@outlook.com>
2018-10-13 20:42:32 +0100

    Added controls for developer list sorting

--- server/static/javascript.js
+++ server/static/javascript.js
@@ -64,3 +64,26 @@ function sortTable() {
     }
   }
 }
+function sortByCount() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("devTable");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+	  console.log(y);
+      if (x.innerHTML < y.innerHTML) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}  
